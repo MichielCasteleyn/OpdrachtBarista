e72b480ad60a574cac06c76a44168dba94e707e3
MichielCasteleyn <michiel.casteleyn@outlook.com>
2019-10-22 21:41:00 +0200

    inladen json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import './style.css';
+import data from './assets/data/coffees.json';
 
 {
   const init = () => {
-    
+    getData(data);
+  };
+
+  const getData = data => {
+    const coffees = data.coffees;
+    coffees.forEach(coffee => {
+
+      if (coffee.plantbased === true) {
+        makeItem(coffee);
+      }
+    });
+  };
+
+  const makeItem = coffee => {
+    const $list = document.querySelector(`.prices__list`);
+
+    const $li = document.createElement(`li`);
+    $li.classList.add(`price`);
+    $li.setAttribute(`data_id`, `${coffee.id}`);
+    $li.innerHTML = `
+      <a class="price__button">
+        <span class="price__button__wrapper">
+          <span class="price__button__name">${coffee.name}</span>
+          <span class="price__button__amount">&euro; ${round(coffee.prices.medium)}</span>
+        </span>
+        <span class="price__button__plus">+</span>
+      </a>`;
+    $list.appendChild($li);
+  };
+
+  const round = num => {
+    return num.toFixed(2);
   };
 
   init();
